2c7cbfe5a8e4e7473dbc569a77699ee4a3013c2f
Randy <wiafe23@gmail.com>
2015-02-18 12:58:49 -0700

    Lets try this again!

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,22 @@
 $( document ).ready(function() {
 
-  //
+  // SmoothScrool Start
+  	smoothScrool(1000);
 
+  function smoothScrool (duration) {
+  	$('a[href^="#"]').on('click', function(event) {
+
+  		var target = $( $(this).attr('href'));
+
+  		if( target.length ) {
+  			event.preventDefault();
+  			$('html, body').animate({
+  				scrollTop: target.offset().top
+  			}, duration);
+  		}
+  	});
+  }
+
+  //SmoothScrool Ending
+  
 });
